5b3c5385459127c8ebda6a1d4a34d62f561d12ff
Chris Samuelson <chris.sam55@gmail.com>
2013-03-05 09:23:43 -0500

    Added some debug in main, and some conditionals to run.

--- main.c
+++ main.c
@@ -3,15 +3,13 @@
 #include <linux/kernel.h>
 #include <linux/etherdevice.h>
 
-struct os_packet
-{
+struct os_packet {
    struct net_device *dev;
    int datalen;
    u8 data[ETH_DATA_LEN];
 };
 
-struct os_priv
-{
+struct os_priv {
    struct net_device_stats stats;
    int status;
    struct os_packet *pkt;
@@ -29,57 +27,62 @@ struct net_device *os1;
 struct os_priv *priv0;
 struct os_priv *priv1;
 
-int os_open(struct net_device *dev)
-{
-   netif_start_queue(dev);
-   return 0;
+int os_open(struct net_device *dev) {
+    netif_start_queue(dev);
+    printk(KERN_INFO "os_open\n");
+    return 0;
 }
 
-int os_stop(struct net_device *dev)
-{
-   netif_start_queue(dev);
-   return 0;
+int os_stop(struct net_device *dev) {
+    netif_stop_queue(dev);
+    printk(KERN_INFO "os_stop\n");
+    return 0;
 }
 
-int os_start_xmit(struct sk_buff *skb, struct net_device *dev){ return 0; }
-
-struct net_device_stats *os_stats(struct net_device *dev)
-{
-   return &(((struct os_priv*)netdev_priv(dev))->stats);
+int os_start_xmit(struct sk_buff *skb, struct net_device *dev){
+    printk(KERN_INFO "os_start_xmit\n");
+    return 0;
 }
 
-static const struct net_device_ops os_device_ops =
-{
-   .ndo_open = os_open,
-   .ndo_stop = os_stop,
-   .ndo_start_xmit = os_start_xmit,
-   .ndo_get_stats = os_stats,
+struct net_device_stats *os_stats(struct net_device *dev) {
+    printk(KERN_INFO "os_stats\n");
+    return &(((struct os_priv*)netdev_priv(dev))->stats);
+}
+
+static const struct net_device_ops os_device_ops = {
+    .ndo_open = os_open,
+    .ndo_stop = os_stop,
+    .ndo_start_xmit = os_start_xmit,
+    .ndo_get_stats = os_stats,
 };
 
 int os_header(struct sk_buff *skb, struct net_device *dev,
               unsigned short type, const void *daddr,
-              const void *saddr, unsigned int len)
-{
-   return 0;
+              const void *saddr, unsigned int len) {
+    struct ethhdr *eth;
+    eth = (struct ethhdr *)skb_push(skb, ETH_HLEN);
+    eth->h_proto = htons(type);
+    memcpy(eth->h_source, dev->dev_addr, dev->addr_len);
+    memcpy(eth->h_dest, eth->h_source, dev->addr_len);
+    eth->h_dest[ETH_ALEN-1] = (eth->h_dest[ETH_ALEN-1] == 5) ? 6 : 5;
+    printk(KERN_INFO "os_header\n");
+    return dev->hard_header_len;
 }
 
-static const struct header_ops os_header_ops = 
-{
+static const struct header_ops os_header_ops = {
    .create = os_header,
 };
 
-int tester1_init (void)
-{
+int tester1_init (void) {
    int i;
 
    os0 = alloc_etherdev(sizeof(struct os_priv));
-   os1 = alloc_etherdev(sizeof(struct os_priv));
-
    priv0 = kmalloc(sizeof(struct os_priv), GFP_KERNEL);
+
+   os1 = alloc_etherdev(sizeof(struct os_priv));
    priv1 = kmalloc(sizeof(struct os_priv), GFP_KERNEL);
 
-   for(i = 0; i < 6; i++)
-   {
+   for(i = 0; i < 6; i++) {
       os0->dev_addr[i] = (unsigned char)i;
       os1->dev_addr[i] = (unsigned char)i;
 
@@ -91,39 +94,29 @@ int tester1_init (void)
    os1->dev_addr[5]++;
 
    os0->hard_header_len = 14;
-   os1->hard_header_len = 14;
-
    memcpy(os0->name, "os0\0", 4);
-   memcpy(os1->name, "os1\0", 4);
-
    os0->netdev_ops = &os_device_ops;
-   os1->netdev_ops = &os_device_ops;
-
    os0->header_ops = &os_header_ops;
-   os1->header_ops = &os_header_ops;
-
    os0->flags |= IFF_NOARP;
-   //os1->flags |= IFF_NOARP;
-
-   memset(priv0, 0, sizeof(struct os_priv));
    memset(priv1, 0, sizeof(struct os_priv));
-
    priv0->dev = os0;
-   priv1->dev = os1;
-
    spin_lock_init(&priv0->lock);
-   spin_lock_init(&priv1->lock);
-
    priv0->rx_int_enabled = 1;
-   priv1->rx_int_enabled = 1;
-
    priv0->pkt = kmalloc(sizeof(struct os_packet), GFP_KERNEL);
-   priv1->pkt = kmalloc(sizeof(struct os_packet), GFP_KERNEL);
-
    priv0->pkt->dev = os0;
-   priv1->pkt->dev = os1;
-
    register_netdev(os0);
+
+   os1->hard_header_len = 14;
+   memcpy(os1->name, "os1\0", 4);
+   os1->netdev_ops = &os_device_ops;
+   os1->header_ops = &os_header_ops;
+   os1->flags |= IFF_NOARP;
+   memset(priv0, 0, sizeof(struct os_priv));
+   priv1->dev = os1;
+   spin_lock_init(&priv1->lock);
+   priv1->rx_int_enabled = 1;
+   priv1->pkt = kmalloc(sizeof(struct os_packet), GFP_KERNEL);
+   priv1->pkt->dev = os1;
    register_netdev(os1);
 
    printk(KERN_INFO "Module loaded\n");
@@ -131,11 +124,9 @@ int tester1_init (void)
    return 0;
 }
 
-void tester1_cleanup(void)
-{
+void tester1_cleanup(void) {
    struct os_priv *priv;
-   if(os0)
-   {
+   if(os0) {
       priv = netdev_priv(os0);
       kfree(priv->pkt);
       unregister_netdev(os0);
@@ -143,8 +134,7 @@ void tester1_cleanup(void)
       kfree(priv0);
    }
 
-   if(os1)
-   {
+   if(os1) {
       priv = netdev_priv(os1);
       kfree(priv->pkt);
       unregister_netdev(os1);
